ad8c98d11a4bb1d4a4a60c26076a784ac577be9a
安西佑介 <anzaiyuusuke@anzaiyuusukenoMacBook-ea.local>
2018-06-04 15:30:11 +0900

    Finish static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  root 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
