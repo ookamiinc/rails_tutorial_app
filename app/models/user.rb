60c9e618f35cf90346b3359ca94a9fda07351f31
Yusuke Anzai <qowgahoh@gmail.com>
2018-06-07 18:07:27 +0900

    Improve intend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-has_many :active_relationships,  class_name:  "Relationship",
+  has_many :active_relationships,  class_name:  "Relationship",
                                  foreign_key: "follower_id",
                                  dependent:   :destroy
-has_many :passive_relationships, class_name:  "Relationship",
+  has_many :passive_relationships, class_name:  "Relationship",
                                  foreign_key: "followed_id",
                                  dependent:   :destroy
-has_many :following, through: :active_relationships,  source: :followed
-has_many :followers, through: :passive_relationships, source: :follower
-
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token ,:reset_token
   before_save :downcase_email
@@ -26,75 +25,71 @@ has_many :followers, through: :passive_relationships, source: :follower
   cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                 BCrypt::Engine.cost
   BCrypt::Password.create(string, cost: cost)
-end
+  end
 
-def User.new_token
+  def User.new_token
    SecureRandom.urlsafe_base64
- end
+  end
 
- def remember
-   self.remember_token = User.new_token
-   update_attribute(:remember_digest, User.digest(remember_token))
- end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
+  def forget
+    update_attribute(:remember_digest,nil)
+  end
 
- def authenticated?(attribute, token)
-   digest = self.send("#{attribute}_digest")
-   return false if digest.nil?
-   BCrypt::Password.new(digest).is_password?(token)
- end
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
- def forget
-   update_attribute(:remember_digest,nil)
- end
-
- def activate
-   update_attribute(:activated,    true)
-   update_attribute(:activated_at, Time.zone.now)
- end
-
- def send_activation_email
-   UserMailer.account_activation(self).deliver_now
- end
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
  # パスワード再設定の属性を設定する
-def create_reset_digest
-  self.reset_token = User.new_token
-  update_attribute(:reset_digest,  User.digest(reset_token))
-  update_attribute(:reset_sent_at, Time.zone.now)
-end
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
 # パスワード再設定のメールを送信する
-def send_password_reset_email
-  UserMailer.password_reset(self).deliver_now
-end
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
-def password_reset_expired?
-  reset_sent_at < 2.hours.ago
-end
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
-def feed
-  following_ids = "SELECT followed_id FROM relationships
-                   WHERE follower_id = :user_id"
-  Micropost.where("user_id IN (#{following_ids})
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
                    OR user_id = :user_id", user_id: id)
- end
+  end
 
-def follow(other_user)
-  active_relationships.create(followed_id:other_user.id)
-end
+  def follow(other_user)
+    active_relationships.create(followed_id:other_user.id)
+  end
 
-def unfollow(other_user)
-   active_relationships.find_by(followed_id: other_user.id).destroy
- end
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 
  # 現在のユーザーがフォローしてたらtrueを返す
  def following?(other_user)
    following.include?(other_user)
  end
-
-
 
  private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
-
-  get '/signup' => 'users#new'
-
 root 'static_pages#home'
-get  '/help' => 'static_pages#help'
-get  '/about' => 'static_pages#about'
-get  '/contact' => 'static_pages#contact'
+get 'password_resets/new'
+get 'password_resets/edit'
+get 'sessions/new'
+get '/signup' => 'users#new'
+get '/help' => 'static_pages#help'
+get '/about' => 'static_pages#about'
+get '/contact' => 'static_pages#contact'
 resources :users do
   member do
     get :following,:followers
@@ -22,6 +18,6 @@ delete '/logout' => 'sessions#destroy'
 resources :account_activations, only:[:edit]
 resources :password_resets, only: [:new,:create,:edit,:update]
 resources :microposts, only: [:create,:destroy]
- resources :relationships,       only: [:create, :destroy]
+resources :relationships, only: [:create, :destroy]
 
 end
