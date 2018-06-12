48b4151f16cdb8683a99dec3a88dbc8cea23405f
Yusuke Anzai <qowgahoh@gmail.com>
2018-06-12 20:57:35 +0900

    creating link to DM room in users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'messages/index'
+
   root 'static_pages#home'
   get 'password_resets/new'
   get 'password_resets/edit'
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
     member do
       get :following, :followers
       get :likes
+      get '/dm' => 'messages#index'
     end
   end
   get '/login' => 'sessions#new'
