67725c9d7b17406cdd23608c16fe6f1301ee8012
Yusuke Anzai <qowgahoh@gmail.com>
2018-06-13 14:29:54 +0900

    improve the arrangement of messages#index

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ApplicationRecord
 
   def self.dm(id1,id2)
     Message.where("send_user_id = ? AND user_id = ?", id1 , id2)
-  #  .or(Message.where("send_user_id = ? AND user_id =?",id2 , id1))
+    .or(Message.where("send_user_id = ? AND user_id =?",id2 , id1))
     end
 
 
