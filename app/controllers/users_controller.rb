26aa4336cbc93c10a22bf1d50c2137f04bb6a1b2
Yusuke Anzai <qowgahoh@gmail.com>
2018-06-15 14:46:36 +0900

    create object for DMusers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @users = current_user.sended_users
     @microposts = @user.microposts.paginate(page: params[:page])
   end
 
