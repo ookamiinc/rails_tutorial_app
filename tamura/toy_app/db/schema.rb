3d1cf21591de3a0cdd6f206c032019fcdc42b257
Kenta Tamura <kt92425@gmail.com>
2021-01-24 09:20:56 +0900

    Add files for micropost

--- tamura/toy_app/db/schema.rb
+++ tamura/toy_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_155203) do
+ActiveRecord::Schema.define(version: 2021_01_24_000503) do
+
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
