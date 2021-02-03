751008fb4d729af9796ced3118c7da693702a60d
Kenta Tamura <kt92425@gmail.com>
2021-02-03 16:35:11 +0900

    Add image posting function

--- tamura/sample_app/test/integration/microposts_interface_test.rb
+++ tamura/sample_app/test/integration/microposts_interface_test.rb
@@ -10,15 +10,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
     assert_select 'a[href=?]', '/?page=2'
     content = "This micropost really ties the room together"
+    image = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpeg')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost:
+                          { content: content, image: image } }
     end
+    assert assigns(:micropost).image.attached?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
